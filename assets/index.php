9eafe48e979b447b15cdd0545a13780aacdb7b94
tonrose <63417569+tonrose@users.noreply.github.com>
2020-04-11 00:05:27 +0800

    commit summary

---
+++ assets/index.php
@@ -0,0 +1,449 @@
+<?php 
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<title>Zion's Praise Music Inc.</title>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href='css/header_footer.css' rel='stylesheet' type='text/css' />
+<link href='css/style_index.css' rel='stylesheet' type='text/css' />
+<link href='css/navi.css' rel='stylesheet' type='text/css' />
+<link rel="icon" href="img/favicon.ico" type="image/x-icon" />
+<link href='css/contact.css' rel='stylesheet' type='text/css' />
+<link href="css/super-panel.css" rel="stylesheet" />
+<script src="js/super-panel.js"></script>
+<script src="js/ccordion-menu.js"></script>
+<script language="JavaScript" src="kaptsa/scripts/gen_validatorv31.js" type="text/javascript"></script>	
+<link href="css/loading.css" rel="stylesheet" />
+  <link href="assets/owl-carousel/owl.carousel.css" rel="stylesheet">
+    <link href="assets/owl-carousel/owl.theme.css" rel="stylesheet">
+	    <link href="assets/owl-carousel/owl.theme2.css" rel="stylesheet">
+<style>#accordion { box-shadow:none; } p{margin:30px 0;}</style>
+
+<style>
+ @font-face {
+  font-family: museo;
+  src: url("css/museo-sans-500.ttf") 
+  }
+  
+ @font-face {
+ font-family: aleg1;
+ src: url("css/Maven Pro Medium.eot") /* EOT file for IE */
+}
+@font-face {
+ font-family: aleg1;
+ src: url("css/Maven Pro Medium.ttf") /* TTF file for CSS3 browsers */
+}
+
+@font-face {
+  font-family: Wingdings 3;
+  src: url("css/WINGDNG3.ttf") 
+  }
+  
+ 
+</style>
+</head>
+<body onload='document.form1a.text1.focus()'>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	
+
+	
+<div id="slider">
+	<div id="owl-demo" class="owl-carousel">
+  <div><img src="img/GSHCS.png" width="100%"></div>
+</div>
+<script>
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+      autoPlay : 20000,
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+ 
+});
+</script>
+
+</div>
+
+<div id="slider2">
+<a href="index.php"><img src="img/m_gshcs.png" alt></a>
+</div>
+	
+<a href="index.php"><input type="submit" id="buttonpopup" value="&#x25B6; PLAY AGAIN" style="position:absolute; 
+width:150px; height:30px; border: solid black 2px; font-family:museo; font-size:10pt; color:black; font-weight:600; background:none;"></a>
+
+<a href="http://musicforall.ph/pmcrecitals/kaibigan.html"><section id="recital"></section></a>
+	
+<div id="main">
+<div class="container">
+
+
+<section id="programs">
+            <div class="container">
+			
+			<div id="wrap">
+			   <a href="pmc.html"><div id="pic39" class="box" >
+      <div class="innerContent">
+      </div>
+    </div></a>
+ <a href="orkidstra.html"><div id="pic38" class="box" >
+      <div class="innerContent">
+      </div>
+    </div></a>
+ <a href="pmctr.html"><div id="pic37" class="box">
+      <div class="innerContent">
+      </div>
+    </div></a>
+	
+
+        </div>
+            </div>
+			
+			
+		
+</ul>
+		
+        </section>
+		
+        <section id="about">
+            <div class="container">
+         <div id="wrap2">
+		 
+	<div id="sub1" class="box2" >
+      <div class="innerContent">
+	  <div id="abt">
+	  <h1><b>About ZPMI</b></h1>
+		<b>Zion<font face="aleg1">'</font>s Praise Music, Inc. is a non-stock,
+		non-profit, and non-denominational music
+		education company that has been partnering
+		with schools all over the Philippines for the
+		past 10 years in providing quality applied
+		music subject programs.</b><br><br>
+		<a href="about.html"><input type="submit" id="button" value="Read More" 
+		style="width:150px; height:30px; border-radius:7px; font-family:museo; 
+		font-size:10pt; color:#0d144a; font-weight:600; background:#fbbc16; box-shadow:none; border:none; "></a>
+      </div>
+	  </div>
+    </div></a>
+	
+ <div id="sub2" class="box2" >
+      <div class="innerContent">
+	    <div id="career">
+		<h1>Careers</h1>
+		<b>We would love you to be a part of our Growing Team 
+		and part of God's mission through Music!
+		Click the button below or send your resume at 
+        <a href="mailto:hr@zpmi.org">hr@zpmi.org</a></b><br><br>
+		<a href="careers.html"><input type="submit" id="button2" value="Browse Job Openings" style="width:150px; height:30px; border-radius:7px; font-family:museo; 
+		font-size:10pt; color:white; background:#0d144a; border:none; " ></a>
+	    </div>
+		</div>
+      </div>
+	  
+
+	  
+    </div>
+
+	
+
+
+        </div>
+            </div>
+        </section>
+		
+		
+		
+        <section id="schools" class="color">
+            <div class="container">
+			
+    <center><H1 id="heading" style="color:#ffffff;font-family:museo;"><b>Find a Partner School near you.</b></H1></center>
+ 	<br>
+	<form name="form1" style="margin-left:260px;">
+	<select name="menu" onChange="location=document.form1.menu.options[document.form1.menu.selectedIndex].value;">
+	<option value=''>Select Region</option>
+	<option value="schools.html?panel=4">Baguio</option>
+	<option value="schools.html?panel=2">Cebu</option>
+	<option value="schools.html?panel=11">Cagayan De Oro</option>
+        <option value="schools.html?panel=3">Davao</option>
+	<option value="schools.html?panel=9">GenSan</option>
+        <option value="schools.html?panel=8">Iloilo</option>
+        <option value="schools.html?panel=5">Isabela</option>
+		<option value="schools.html?panel=10">Koronadal</option>
+        <option value="schools.html?panel=7">Laguna</option>
+	<option value="schools.html?panel=6">Cavite</option>
+        <option value="schools.html?panel=0">Manila, North</option>
+        <option value="schools.html?panel=1">Manila, South</option>
+	</select>
+	</form> 
+	
+            </div>
+        </section>
+		
+		<iframe id="map" width="120%" height="320px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" style="margin-top:0px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1607.2203659523977!2d121.01117859965987!3d14.461189007769331!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTTCsDI3JzQwLjUiTiAxMjHCsDAwJzQzLjMiRQ!5e1!3m2!1sen!2sph!4v1481386283274"></iframe>
+              
+	
+	<section id="contactus" class="contactus">
+	
+            <div class="container">   
+			
+			    
+                                 <!-- feedback form starts here -->
+ <div id="feedback">
+  <!-- heading of the form -->
+	<div class="head">
+	<div id="header">
+    <center><font style="color:#0c035e;font-family:museo; font-size:20pt;"><b>&nbsp;&nbsp;We would love to hear from you!</b></center>
+	  <center><font style="color:#0c035e;font-family:museo; font-size:12pt;"><b>Once we receive your messages, we will contact you back very soon. All fields below are required.</b></font></center>
+    </div>
+	
+	<!-- feedback form form -->
+        <?php include('kaptsa/html-contact-form.php'); ?>
+        <!--
+		<div class="mail">
+		<form name="form1a" method="post" action="contactengine.php">
+		  </?php
+          //require_once('recaptchalib.php');
+          //$publickey = "6LdbrAwTAAAAAChQSu-O6c3qE4wUxCR9zYNrNbaZ"; // you got this from the signup page
+		  //echo recaptcha_get_html($publickey);
+          ?>
+
+				<input type="text" name="Name" id="Name"  placeholder="Your Name" required autofocus />
+				<span class="error"></?php echo $Name;?></span>
+				<br>
+				<input type="text" name="Subject" id="Subject"  placeholder="Your Subject" required autofocus />
+				<span class="error"></?php echo $Subject;?></span>
+				<br>
+				<input type="text" name="text1" id="Email"  placeholder="Your E-mail" required autofocus />
+				<span class="error"></?php echo $Email;?></span>
+				<br>
+				<textarea name="Message" rows="20" cols="20" id="Message"  placeholder="Your Message" required autofocus ></textarea>
+				<span class="error"></?php echo $Message;?></span>
+				<br>
+                <!--<center><div class="g-recaptcha" data-sitekey="6LdbrAwTAAAAAChQSu-O6c3qE4wUxCR9zYNrNbaZ" ></div></center>-->
+
+		</form>
+		
+		
+     </div> 
+ <!-- feedback form ends here -->
+            </div>
+        </section>
+	
+	
+	
+	
+</div>	
+
+<footer>
+<div id="info-bar">
+    <div class="container clearfix">
+        <span class="all-tutorials"><a href="about.html">See our Branches. Zion's Praise Music Inc. 2017.</a>  </span>
+        <span class="back-to-tutorial"> <a href="https://www.facebook.com/ZionsPraise?fref=ts"><img src="img/facebook-4-256.png" width="36px" height="36px"></a><a href="https://twitter.com/zpmi"><img src="img/twitter-256.png" width="36px" height="36px"></a></span>
+    </div>
+</div><!-- /#top-bar -->
+</footer><!-- /footer -->   
+	
+	
+	 </div>
+ </div>
+	
+<div id="popup1" class="overlay">
+	<div class="popup">
+		
+		<a class="close" href="#">CLOSE</a>
+		<div class="content" id="yt-player">
+		 <iframe width="853" height="480" src="https://www.youtube.com/embed/ccf2GSfoezE?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+		</div>
+	</div>
+</div>
+	
+	
+	
+<script>
+	$(document).ready(function() {
+    $(function(){
+      
+      $('#popup1').css("visibility", "visible"); 
+       $('#popup1').css("opacity", 1); 
+      
+       });
+  
+  $( ".close" ).click(function() {
+    
+       $('#popup1').css("visibility", "hidden"); 
+       $('#popup1').css("opacity", 0);
+	   $('#popup1 iframe').attr('src', '');
+    });
+});
+	</script>
+	
+	
+<div id="head">
+
+  <a href="#slider"><img id="zpmilogo" src="img/1.png"></a>
+  
+ 
+  <div id="nav">
+	    <ul class="main-navigation">
+   <li><a href="#">What's Inside?</a>
+        <ul>
+          <li><a href="about.html">About Us</a></li>
+          <li><a href="schools.html">Our Partner Schools</a></li>
+		  <li><a href="careers.html">Careers</a></li>
+		  <li><a href="pmctr.html">PMCenter</a></li>
+		  <li><a href="media.html">Media</a></li>
+		  <li><a href="http://www.musicforall.ph/blog/">Blog</a></li>
+        </ul>
+      </li>
+  <li><a href="index.html#programs">Programs </a>
+    <ul class="slideInDown">
+      <li><a href="pmc.html">PMC</a>
+	    <ul>
+          <li><a href="wow_roadtrack.html">WOW Roadtrack</a></li>
+		   <li><a href="pmcrecitals.html">Recitals</a></li>
+        </ul>
+	  </li>
+      <li><a href="orkidstra.html">Orkidstra</a>
+      </li>
+      <li><a href="#">Summer Programs</a>
+        <ul>
+          <li><a href="dvms.html">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;DVMS&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+        </ul>
+      </li>
+	  <li><a href="ileap.html">iLEAP</a></li>
+    </ul>
+  </li>
+
+  <li><a href="https://www.facebook.com/topsoundmusicinc?ref=tn_tnmn" target="_blank">TOP SOUND</a></li>
+     <li><a href="#contactus">Contact</a></li>
+    <li><a href="http://www.musicforall.ph/system/signin.php">Log In</a></li>
+  </ul>
+	</div>  	 
+	
+	
+	
+	
+	
+	
+	<div id="nav2">
+      <header>
+        <span data-panel="panel1" class="panel-button"></span>      
+    </header>
+       <a href="index.html"><img id="zpmilogo2" src="img/1.png"></a>
+     <div id="panel1">
+        <div style="text-align:center;margin:10px 0;">
+            <img src="img/ZPMI New Logo 2 transparent.png" width="110" height="145" />
+        </div>
+        <div id="accordion">
+            <ul>
+			    <li>
+                    <a href="#"><span style="font-family: 'Wingdings 3'">&#x25B6;</span>&nbsp;&nbsp;What's Inside?</a>
+                    <ul>
+                        <li><a href="about.html">About Us</a></li>
+                        <li><a href="schools.html">Partner Schools</a></li>
+                        <li><a href="careers.html">Careers</a></li>
+						<li><a href="pmctr.html">PMCenter</a></li>
+						<li><a href="media.html">Media</a></li>
+
+                    </ul>
+                </li>
+				<br>
+			
+                <li>
+                    <a href="#"><span style="font-family: 'Wingdings 3'">&#x25B6;</span>&nbsp;&nbsp;Programs</a>
+                    <ul>
+                        <li><a href="pmc.html">PMC</a>
+						<ul>
+                                <li><a href="wow_roadtrack.html">W.O.W Roadtrack</a></li>
+								<li><a href="pmcrecitals.html">Recitals</a></li>
+                            </ul>
+						</li>
+                        <li><a href="orkidstra.html">Orkidstra</a></li>
+						 <li>
+                            <a href="#">Summer Programs</a>
+                            <ul>
+                                <li><a href="dvms.html">DVMS</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="ileap.html">ILEAP</a>
+                  
+                        </li>
+                    </ul>
+                </li>
+				<br>
+                
+                <li>
+                   <a href="https://www.facebook.com/topsoundmusicinc?ref=tn_tnmn"><span style="font-family: 'Wingdings 3'">&#x25B6;</span>&nbsp;&nbsp;TOP SOUND</a>  
+                </li>
+				<br>
+                <li>
+                    <a href="#contactus"><span style="font-family: 'Wingdings 3'">&#x25B6;</span>&nbsp;&nbsp;Contact</a>
+                </li>
+				<br>
+				<li><a href="http://www.musicforall.ph/blog/"><span style="font-family: 'Wingdings 3'">&#x25B6;</span>&nbsp;&nbsp;Blog</a></li>
+				<br>
+				<li>
+                    <a href="http://www.musicforall.ph/system/signin.php"><span style="font-family: 'Wingdings 3'">&#x25B6;</span>&nbsp;&nbsp;Log In</a>
+                </li>
+            </ul>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+			<div id="sms">
+			<a href="https://www.facebook.com/ZionsPraise?fref=ts"><img src="img/facebook-4-256 - Copy.png" width="36px" height="36px"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://twitter.com/zpmi"><img src="img/twitter-256 - Copy.png" width="36px" height="36px"></a>
+            </div>
+		</div>
+    </div>
+
+ 
+
+
+	
+<script src="email-validation.js"></script>
+<script src="owl-carousel/owl.carousel.js"></script>
+
+    <script type="text/javascript">
+        $(document).scroll(function() {
+  if ($(this).scrollTop() > 10) { //Adjust 150
+    $('#head').addClass('shrinked');
+  } else {
+    $('#head').removeClass('shrinked');
+  }
+});
+    </script>
+	
+	<script>
+	 $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+	</script>	
+
+</body>
+
+</html>
